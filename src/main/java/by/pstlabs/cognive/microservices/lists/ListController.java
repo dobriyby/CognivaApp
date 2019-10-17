085d42efcbf2f0bbd4dae408619f6b13f2afa8e1
Yuri Korzun <yury.korzun@pstlabs.by>
2019-10-17 17:53:10 +0300

    include request moment in response

--- src/main/java/by/pstlabs/cognive/microservices/lists/ListController.java
+++ src/main/java/by/pstlabs/cognive/microservices/lists/ListController.java
@@ -3,11 +3,13 @@ package by.pstlabs.cognive.microservices.lists;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class ListController {
 
     @GetMapping("/echo")
     private String echo(){
-        return "I`m alive!";
+        return "I`m alive! Current time is " + new Date();
     }
 }
