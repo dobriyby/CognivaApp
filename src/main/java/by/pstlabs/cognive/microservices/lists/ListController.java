f4768a6b277b3663327e4e634fd85d090bb14d5c
Yuri Korzun <yury.korzun@pstlabs.by>
2019-10-17 17:42:42 +0300

    rest endpoint

---
+++ src/main/java/by/pstlabs/cognive/microservices/lists/ListController.java
@@ -0,0 +1,13 @@
+package by.pstlabs.cognive.microservices.lists;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListController {
+
+    @GetMapping("/echo")
+    private String echo(){
+        return "I`m alive!";
+    }
+}
