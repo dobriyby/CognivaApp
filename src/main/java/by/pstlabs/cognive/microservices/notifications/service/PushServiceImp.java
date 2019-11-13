cded0dc2032b2c7ede88c725f8c5f87c3c8008e1
mage-wow@mail.ru <mage7313>
2019-11-13 13:32:35 +0300

    minor update

--- src/main/java/by/pstlabs/cognive/microservices/notifications/service/PushServiceImp.java
+++ src/main/java/by/pstlabs/cognive/microservices/notifications/service/PushServiceImp.java
@@ -1,16 +1,17 @@
 package by.pstlabs.cognive.microservices.notifications.service;
 
+import by.pstlabs.cognive.common.model.User;
+import by.pstlabs.cognive.microservices.notifications.model.ApiResponse;
 import by.pstlabs.cognive.microservices.notifications.model.MailNotificationRequest;
 import by.pstlabs.cognive.microservices.notifications.model.Push;
 import by.pstlabs.cognive.microservices.notifications.repository.PushRepository;
-import by.pstlabs.cognive.microservices.userlist.model.User;
 import by.pstlabs.cognive.microservices.userlist.repository.UserRepository;
-import org.simplejavamail.mailer.Mailer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class PushServiceImp implements PushService {
         List<Push> pushes = pushRepo.findPushesBySendtimeBeforeAndSendStatusFalse(new Date());
         for (Push push: pushes) {
             for (User user: push.getUsers()) {
-                mailer.SendMail(new MailNotificationRequest(user.getEmail(),"Cogniva Mail Natification",user.getName(),push.getMessage()));
+                mailer.SendMail(new MailNotificationRequest(user.getEmail(),"test",user.getName(),push.getMessage()));
             }
             push.setSendStatus(true);
             pushRepo.save(push);
@@ -40,27 +41,32 @@ public class PushServiceImp implements PushService {
     }
 
     @Override
-    public String addPushToAll(Date date, String text) {
+    public ResponseEntity<Object> addPushToAll(Date date, String text) {
         Push push = new Push();
         push.setSendtime(date);
         push.setUsers(userRepo.findAll());
         push.setSendStatus(false);
         push.setMessage(text);
         pushRepo.save(push);
-        return "done";
+        ApiResponse apiResponse =
+                new ApiResponse(HttpStatus.OK, "Push success added to database!", 0, "");
+        return new ResponseEntity<>(
+                apiResponse, new HttpHeaders(), apiResponse.getStatus());
     }
 
     @Override
-    public String addPushToUserName(Date date, String name, String text) {
-
+    public ResponseEntity<Object> addPushToUserName(Date date, String name, String text) {
         Push push = new Push();
         push.setSendStatus(false);
         push.setMessage(text);
         push.setSendtime(date);
         push.setUsers(userRepo.findAllByName(name));
         pushRepo.save(push);
-        return "done";
+        ApiResponse apiResponse =
+                new ApiResponse(HttpStatus.OK, "Push success added to database!", 0, "");
+        return new ResponseEntity<>(
+                apiResponse, new HttpHeaders(), apiResponse.getStatus());
     }
 
-
 }
+
