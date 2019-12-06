21238d043beaad34accfdc22d85c333a5c9ed5f8
mage-wow@mail.ru <Mage7313>
2019-12-06 14:52:32 +0300

    test commit

--- src/main/java/by/pstlabs/cognive/microservices/notifications/service/TelegramBotService.java
+++ src/main/java/by/pstlabs/cognive/microservices/notifications/service/TelegramBotService.java
@@ -96,7 +96,6 @@ public class TelegramBotService{
             case ("/camerashot"):{
                 Webcam webcam = Webcam.getDefault();
                 webcam.open();
-                System.out.println("test3");
                 bot.sendImage(user,webcam.getImage());
                 webcam.close();
                 log.info("Send webcam screenshot to "+update.getMessage().getFrom().getFirstName());
